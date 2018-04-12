8fa786736f17d6c6442b08f1948ff9e278c201b9
Ruaridh <ruaridhdunbar@gmail.com>
2018-04-12 15:41:37 +0100

    correct syntax errors within codeclan_cinema.sql

--- db/codeclan_cinema.sql
+++ db/codeclan_cinema.sql
@@ -4,7 +4,7 @@ DROP TABLE films;
 
 CREATE TABLE customers (
   id SERIAL8 PRIMARY KEY,
-  name VARCHAR(255),
+  name VARCHAR(255)
 );
 
 CREATE TABLE films (
@@ -16,5 +16,5 @@ CREATE TABLE films (
 CREATE TABLE tickets (
   id SERIAL8 PRIMARY KEY,
   customer_ID INT8 REFERENCES customers(id) ON DELETE CASCADE,
-  film_id INT8 REFERENCES films(id) ON DELETE CASCADE,
-)
+  film_id INT8 REFERENCES films(id) ON DELETE CASCADE
+);
