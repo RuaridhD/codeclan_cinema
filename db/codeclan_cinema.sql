616ac8896e0a8ec2290d072c50bb8163fc781523
Ruaridh <ruaridhdunbar@gmail.com>
2018-04-12 15:32:48 +0100

    create sql file for cinema

---
+++ db/codeclan_cinema.sql
@@ -0,0 +1,20 @@
+DROP TABLE tickets;
+DROP TABLE customers;
+DROP TABLE films;
+
+CREATE TABLE customers (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+);
+
+CREATE TABLE films (
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT
+);
+
+CREATE TABLE tickets (
+  id SERIAL8 PRIMARY KEY,
+  customer_ID INT8 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT8 REFERENCES films(id) ON DELETE CASCADE,
+)
